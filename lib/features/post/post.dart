d021f119b92081e0b55393533c2cd284f9d9fc9e
Railan Santana <127506906+LSthemagic@users.noreply.github.com>
2026-07-18 17:10:57 -0300

    feat: enhance PostPage with input field and publish button

--- lib/features/post/post.dart
+++ lib/features/post/post.dart
@@ -22,6 +22,43 @@ class PostPage extends StatelessWidget {
           ),
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 13, right: 12),
+        child: SizedBox(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLines: 10,
+                maxLength: 280,
+                decoration: const InputDecoration(
+                  hintText: 'O que você está pensando?',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 48),
+                  backgroundColor: Colors.deepPurple,
+                ),
+                child: const Text(
+                  "Publicar",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              SizedBox(height: 8),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Escreva uma mensagem simples e direta.",
+                  style: TextStyle(color: Colors.grey),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
