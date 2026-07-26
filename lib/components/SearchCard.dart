1bb266cb0c7e97aa55ce145842d8f05f6547b287
Railan Santana <127506906+LSthemagic@users.noreply.github.com>
2026-07-26 20:23:58 -0300

    feat: add SearchCard component and integrate it into SearchPage

---
+++ lib/components/SearchCard.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class SearchCard extends StatelessWidget {
+  const SearchCard({super.key, this.onTap});
+
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 13, right: 12, top: 8, bottom: 8),
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey.shade300),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            const CircleAvatar(
+              radius: 18,
+              backgroundColor: Color.fromARGB(255, 228, 215, 253),
+            ),
+
+            const SizedBox(width: 10),
+
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Maria Silva',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+                  ),
+                  Text(
+                    '@maria',
+                    style: TextStyle(color: Colors.grey.shade600, fontSize: 11),
+                  ),
+                ],
+              ),
+            ),
+
+            FilledButton(
+              onPressed: onTap,
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 0,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.only(
+                    topLeft: Radius.circular(8),
+                    bottomLeft: Radius.circular(8),
+                    bottomRight: Radius.circular(8),
+                    topRight: Radius.circular(8),
+                  ),
+                  side: BorderSide(color: Colors.blueGrey.shade50),
+                ),
+              ),
+              child: const Text(
+                'Perfil',
+                style: TextStyle(color: Colors.black54),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/search/search.dart
+++ lib/features/search/search.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:papacapim/components/AppBar.dart';
+import 'package:papacapim/components/SearchCard.dart';
+import 'package:papacapim/features/post/details.dart';
 
-class SearchPage extends StatelessWidget{
+class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Text("salve"),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(title: Text("Buscar")),
+      body:ListView.builder(
+          itemCount: 4,
+          itemBuilder: (context, index) {
+            return SearchCard(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const DetailsPostPage()),
+                );
+              },
+            );
+          },
+      ),
     );
   }
-
 }
