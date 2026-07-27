806b65a86779aab91aab8b5d4d5641312a4999fa
Railan Santana <127506906+LSthemagic@users.noreply.github.com>
2026-07-26 22:13:09 -0300

    fix: update padding in ProfilePage for consistency

--- lib/features/profile/profile.dart
+++ lib/features/profile/profile.dart
@@ -24,7 +24,7 @@ class ProfilePage extends StatelessWidget {
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(13.0),
+        padding: const EdgeInsets.all(12.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
