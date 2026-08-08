70e9b668c2c97036da7830b159e9ef1ac5f71fcc
Railan Santana <127506906+LSthemagic@users.noreply.github.com>
2026-08-08 20:18:32 -0300

    feat: implement AppSnackBar for user feedback and update RegisterPage to use it

--- lib/features/auth/register.dart
+++ lib/features/auth/register.dart
@@ -1,125 +1,168 @@
 import 'package:flutter/material.dart';
 import 'package:papacapim/components/app_bar.dart';
+import 'package:papacapim/core/utils/app_snackbar.dart';
+import 'package:papacapim/features/auth/controllers/register_controller.dart';
 import 'package:papacapim/features/auth/login.dart';
 import 'package:papacapim/navigation/navigation.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: CustomAppBar(
-          title: Text("Criar Conta"),
-          actions: [
-            TextButton(onPressed: () {
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final loginController = TextEditingController();
+  final nameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final passwordConfirmationController = TextEditingController();
+
+  final registerController = RegisterController();
+
+  Future<void> register() async {
+    print("ta aq");
+    final success = await registerController.register(
+      login: loginController.text,
+      name: nameController.text,
+      password: passwordController.text,
+      passwordConfirmation: passwordConfirmationController.text,
+    );
+
+    if (!mounted) return;
+
+    success
+        ? AppSnackBar.show(context, "Cadastro Realizado!")
+        : AppSnackBar.show(
+            context,
+            registerController.error ?? 'Erro ao cadastrar',
+          );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    registerController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    loginController.dispose();
+    nameController.dispose();
+    passwordController.dispose();
+    passwordConfirmationController.dispose();
+    registerController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        title: Text("Criar Conta"),
+        actions: [
+          TextButton(
+            onPressed: () {
               Navigator.pushReplacement(
                 context,
-                MaterialPageRoute(builder: (_) => const LoginPage())
+                MaterialPageRoute(builder: (_) => const LoginPage()),
               );
-            }, child: const Text("Entrar"))
-          ],
-        ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(12.0),
-            child: Form(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Nome",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Digite seu nome",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
+            },
+            child: const Text("Entrar"),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(12.0),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Nome",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Digite seu nome",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
                     ),
                   ),
-                  const SizedBox(height: 8),
-                  const Text(
-                    "Login",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "seu_login",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  "Login",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "seu_login",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
                     ),
                   ),
-                  const SizedBox(height: 8),
-                  const Text(
-                    "Senha",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      hintText: "*********",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  "Senha",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "*********",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
                     ),
                   ),
-                  const SizedBox(height: 24),
-                  const Text(
-                    "Confirmar senha",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      hintText: "*********",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
+                ),
+                const SizedBox(height: 24),
+                const Text(
+                  "Confirmar senha",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "*********",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
                     ),
                   ),
-                  const SizedBox(height: 24),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => const NavigationPage(),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.deepPurple,
-                        minimumSize: const Size(double.infinity, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      child: const Text(
-                        "Cadastrar",
-                        style: TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 24),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: registerController.isLoading ? null : register,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple,
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
+                    child: registerController.isLoading ? const CircularProgressIndicator() :  const Text(
+                      "Cadastrar",
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
       ),
-      
     );
   }
-
-
 }
--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -2,26 +2,36 @@ import 'package:dio/dio.dart';
 import 'package:papacapim/core/api/api_client.dart';
 
 class AuthService {
+
+  String parseErrors(dynamic data) {
+  if (data is Map) {
+    return data.entries
+        .map((e) => '${e.key}: ${(e.value as List).join(', ')}')
+        .join('\n');
+  }
+
+  return data.Exception.toString();
+}
+
   Future<void> register({
     required String login,
     required String name,
     required String password,
     required String passwordConfirmation,
   }) async {
-    try{
+    try {
       await ApiClient.dio.post(
         "/users",
         data: {
           "login": login,
           "name": name,
           "password": password,
-          "password_confirmation": passwordConfirmation
-        }
+          "password_confirmation": passwordConfirmation,
+        },
       );
-    }on DioException catch (e){
-       throw Exception(
-        e.response?.data['message'] ?? 'Erro ao criar usuário',
-      );
+    } on DioException catch (e) {
+      print(e.response);
+      throw Exception(parseErrors(e.response?.data));
     }
   }
 }
